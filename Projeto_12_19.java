64e8f7a4812555ca494d57f35f7c6e54b683cdd6
Wesley Pedreira Passos Rocha <wesley.p.rocha6@aluno.senai.br>
2024-08-16 21:42:14 -0300

    Add files via upload

---
+++ Projeto_12_19.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_12_9;
+
+/**
+ *
+ * @author w.rocha
+ */
+import java.util.Scanner;
+public class Projeto_12_9 {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+         int base = 0;
+          int expoente = 0;
+           int resultado = 0;
+          
+          System.out.println("Digite a base");
+           base = ler.nextInt();
+           
+          System.out.println("Digite o expoente");
+           expoente = ler.nextInt();
+          
+           System.out.pr
+        
+    }
+}
