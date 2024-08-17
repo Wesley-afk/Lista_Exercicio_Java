ea8712f26eee0957e6f5d4a3cb4e71518793ab6e
Wesley Pedreira Passos Rocha <wesley.p.rocha6@aluno.senai.br>
2024-08-16 21:42:58 -0300

    Add files via upload

---
+++ Projeto_12_20.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_12_20;
+
+/**
+ *
+ * @author w.rocha
+ */
+public class Projeto_12_20 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
