7c97a1298ad73b2ad5ebf90024fe2c19b56ffd45
Wesley Pedreira Passos Rocha <wesley.p.rocha6@aluno.senai.br>
2024-08-17 15:41:25 -0300

    Update Projeto_12_20.java

--- Projeto_12_20.java
+++ Projeto_12_20.java
@@ -8,9 +8,25 @@ package com.mycompany.projeto_12_20;
  *
  * @author w.rocha
  */
+import java.util.Scanner;
 public class Projeto_12_20 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner ler = new Scanner(System.in);
+         int compra = 0;
+          double desconto = 0;
+           double porcentagem_desconto = 0;
+          
+          System.out.println("Digite o valor total da compra.");
+           compra = ler.nextInt();
+           
+          System.out.println("Digite a porcentagem de desconto.");
+           desconto = ler.nextInt();
+           
+           if(desconto > 0 && desconto <= 100){
+             porcentagem_desconto = (desconto / 100) * compra;
+               System.out.println("O valor a ser pago é de: " + (compra - porcentagem_desconto));
+           }
+           
     }
 }
